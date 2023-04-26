07ebffb8d030f9fbcd9e55e6ca434c77d832b976
ishtylerc <ishtyler@gmail.com>
2023-04-26 18:34:16 -0400

    code change

--- create_connection.py
+++ create_connection.py
@@ -4,8 +4,8 @@ def main():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     host = 'localhost'
     port = 5000
-    s.connect((host, port))
-    print('It works')
+    result = s.connect_ex((host, port))
+    print('Results is {}'.format(result))
 
 if __name__ == '__main__':
     main()
